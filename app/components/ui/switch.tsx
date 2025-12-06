bf587e90378f51cf5888a88816680470f054a00d
FadlanBM <fadlankurahan2@gmail.com>
2025-12-06 09:32:16 +0700

    feat : components

--- app/components/ui/switch.tsx
+++ app/components/ui/switch.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import * as React from "react";
@@ -27,29 +26,10 @@ const Switch = React.forwardRef<
       ref={ref}
       className={cn(
         "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
-=======
-"use client"
-
-import * as React from "react"
-import * as SwitchPrimitive from "@radix-ui/react-switch"
-
-import { cn } from "@/app/lib/utils"
-
-function Switch({
-  className,
-  ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
-  return (
-    <SwitchPrimitive.Root
-      data-slot="switch"
-      className={cn(
-        "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
->>>>>>> 11b291b (feat : genenerate text)
         className
       )}
       {...props}
     >
-<<<<<<< HEAD
       <span
         data-state={checked ? "checked" : "unchecked"}
         className={cn(
@@ -62,16 +42,3 @@ function Switch({
 Switch.displayName = "Switch";
 
 export { Switch };
-=======
-      <SwitchPrimitive.Thumb
-        data-slot="switch-thumb"
-        className={cn(
-          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
-        )}
-      />
-    </SwitchPrimitive.Root>
-  )
-}
-
-export { Switch }
->>>>>>> 11b291b (feat : genenerate text)
